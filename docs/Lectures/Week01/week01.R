01ca1bb3af27d935dc0cec9c02fe3b212dc5fbde
Nancy <ywu120766@gmail.com>
2024-12-22 23:05:38 -0500

    update download link for week 01

--- docs/Lectures/Week01/week01.R
+++ docs/Lectures/Week01/week01.R
@@ -1,3 +1,5 @@
+# Working directory
+
 # get working directory
 getwd()
 
@@ -5,13 +7,17 @@ getwd()
 setwd('D:\\Teaching_Clark\\GitRepo\\Spring2025\\geog-247\\docs\\Lectures\\Week01')
 getwd()
 
+
 # Terminate script
+
 i <- 1
 while (i>0) {
   print('good')
 }
 
-# get help for activate libraries
+
+# Get help for activate libraries
+
 help('dplyr')
 ?dplyr
 
@@ -19,7 +25,9 @@ help('dplyr')
 ??dplyr
 help.search('dplyr')
 
-# Assignment operator
+
+# Variables
+
 x <- 10 
 y <- 10 
 sum <- x + y  # Add x and y
@@ -29,7 +37,9 @@ print(x)  # Output the value of x
 print(sum)  # Output the sum
 print(product)  # Output the product
 
-# Changing Variable Values
+
+## Changing Variable Values
+
 x <- 15  # Reassign a new value to x
 print(x)  # Output the updated value of x
 
@@ -38,19 +48,8 @@ name <- "R Programming"  # Character
 is_great <- TRUE         # Logical
 pi_value <- 3.14         # Numeric
 
-# clean environment
-rm(list=ls())
 
-# check columns
-# Display column names
-data = read.csv('parks_trees')
-colnames(data)
-
-# Inspecting a specific column
-data$neighborhood
-
-# add new columns
-data$data_source <- "Boston_GIS"
+# List
 
 #creating a list
 my_list <- list(
@@ -72,8 +71,14 @@ print(my_list[["age"]])  # Outputs: 30
 # Using $
 print(my_list$scores)  # Outputs: c(85, 90, 95)
 
-# read csv
-# Reading a CSV file
+
+# Clean environment
+
+rm(list=ls())
+
+
+# Read csv
+
 data <- read.csv("parks_trees.csv")
 
 # Display the first few rows of the dataset
@@ -86,4 +91,13 @@ str(data)
 summary(data)
 
 
+## Display column names
+
+colnames(data)
+
+# Inspecting a specific column
+data$neighborhood
+
+# add new columns
+data$data_source <- "Boston_GIS"
 
