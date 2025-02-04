1ac92ec81659ff1603584db929cf6955e00ed379
YANAN WU <ywu120766@gmail.com>
2025-02-04 13:11:52 -0500

    update week04 lecture transformation

--- docs/Labs/Lab02/Lab02_SampleCode.R
+++ docs/Labs/Lab02/Lab02_SampleCode.R
@@ -7,14 +7,27 @@ un_model = lm(infantMortality~ppgdp, data = UN)
 plot(un_model)
 
 ## check skewness in independent variable and dependent variable
-scatterplot(infantMortality~ppgdp, data = UN,xlab = 'Gross Domestic Product per Capita', 
+car::scatterplot(infantMortality~ppgdp, data = UN,xlab = 'Gross Domestic Product per Capita', 
             ylab = 'Infant Mortality Rate (per 1000 births)', 
             regLine = list(col = 'darkgreen'), 
             smooth = list(col.smooth = "red"))
 
 
-## estimate lambda for dependent variable
-p1 = powerTransform(infantMortality~1, data = UN, family = 'bcPower')
+## estimate if transformation needed for ppgdp
+p1 = powerTransform(ppgdp~1, data = UN, family = 'bcPower')
 summary(p1)
 
-## 
+## examine the data distribution for ppgdp based on three different value of lambda
+par(mfrow = c(1,3))
+hist(car::bcPower(UN$ppgdp, lambda=0),breaks = 12,main = 'lambda = 0',xlab = 'x')
+hist(car::bcPower(UN$ppgdp, lambda=0.026),breaks = 12,main = 'lambda = 0.026',xlab = 'x')
+hist(car::bcPower(UN$ppgdp, lambda=-1),breaks = 12,main = 'lambda = -1',xlab = 'x')
+
+## estimate if transformation needed for infantMortality
+p2 = powerTransform(infantMortality~log(ppgdp), data = UN, family = 'bcPower')
+summary(p2)
+
+
+summary(powerTransform(infantMortality~log(ppgdp), data = UN, family = 'bcPower'))
+
+ 
