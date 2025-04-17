69b3165c2d606225d45f008ab3a2af4634603cf0
Nancy <ywu120766@gmail.com>
2025-04-17 09:29:34 -0400

    update files

--- docs/Labs/Lab07/Lab07SampleAnswer.R
+++ docs/Labs/Lab07/Lab07SampleAnswer.R
@@ -13,9 +13,6 @@ plotRGB(landsat, r = 4, g = 3, b = 2, stretch = "lin", main = "False Color Compo
 summary_stats <- global(landsat, fun = c("min", "max", "mean", "sd"))
 summary_stats
 
-landsat_sta <- scale(landsat)
-summary_stats <- global(landsat_sta, fun = c("min", "max", "mean", "sd"))
-summary_stats
 landsat_pca <- rasterPCA(landsat)
 
 # Examine the summary of the PCA results
@@ -32,9 +29,11 @@ print(paste("Variance explained by each PC:", paste(round(explained_var * 100, 2
 par(mfrow = c(2, 2))
 plot(landsat_pca$map$PC1, main = "Principal Component 1", col = gray.colors(100))
 plot(landsat_pca$map$PC2, main = "Principal Component 2", col = gray.colors(100))
-
+plot(landsat_pca$map$PC3, main = "Principal Component 3", col = gray.colors(100))
+plot(landsat_pca$map$PC4, main = "Principal Component 4", col = gray.colors(100))
 ##Task 5
 # Create an RGB composite using the first three
+par(mfrow = c(1, 1))
 plotRGB(landsat_pca$map, r = 1, g = 2, b = 3, stretch = "lin", 
         main = "RGB Composite of First Three Principal Components")
 
